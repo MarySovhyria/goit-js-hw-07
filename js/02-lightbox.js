00f681e9b4fc4332f0423ebbd60047a63fce7afb
MarySovhyria <111424962+MarySovhyria@users.noreply.github.com>
2023-08-09 14:04:44 +0100

    Add files via upload

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,26 +3,17 @@ import { galleryItems } from './gallery-items.js';
 
 const container = document.querySelector(".gallery")
 const markup = galleryItems.map(({preview, original, description}) => 
-`<li>
-    <a href = "${original}">
-        <img class = "js-target" data-source = "${original} " src = "${preview}" alt = "${description}">
-    </a>
-</li>`
+`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>
+
+
+`
 )
 container.insertAdjacentHTML('beforeend', markup.join(""))
-container.addEventListener('click', onclick)
 
-function onclick(event) {
-    event.preventDefault();
-    if (!event.target.classList.contains('js-target')) {
-        return
-    }
-    const picture = event.target.dataset.source;
-    const instance = basicLightbox.create(`
-    <div class = "modal"><img src="${picture}" ></div>
-`);
-    instance.show()
-}
 var lightbox = new SimpleLightbox('.gallery a', { 
     captionsData: "alt",
     captionsDelay: 250,
